1f2663b302e315d5c6ef653cab5cf45ea3608166
Leonardo Droves <leodroves@hotmail.com>
2023-08-04 19:32:06 +0000

    Install `Prettier`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Um clone do site https://www.tabnews.com.br/ para treinar
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,18 @@
 function Home() {
-    return (
-        <>
-            <h1>What's the opposite of these words?</h1>
-            <ul>
-                <li>Always</li>
-                <li>Coming</li>
-                <li>From</li>
-                <li>Take</li>
-                <li>Me</li>
-                <li>Down</li>
-            </ul>
-            <a href="https://www.youtube.com/shorts/41iWg91yFv0">Answer</a>
-        </>
-    )
+  return (
+    <>
+      <h1>What's the opposite of these words?</h1>
+      <ul>
+        <li>Always</li>
+        <li>Coming</li>
+        <li>From</li>
+        <li>Take</li>
+        <li>Me</li>
+        <li>Down</li>
+      </ul>
+      <a href="https://www.youtube.com/shorts/41iWg91yFv0">Answer</a>
+    </>
+  );
 }
 
-export default Home
+export default Home;
